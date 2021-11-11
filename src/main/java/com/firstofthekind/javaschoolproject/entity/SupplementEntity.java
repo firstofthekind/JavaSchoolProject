8dbcbf0cc5d4acf5dd2e7875a740e1590cc54721
Viacheslav <Firstofthekind@yandex.ru>
2021-11-11 21:47:19 +0300

    Incompatible and codependent supplement methods added.

--- src/main/java/com/firstofthekind/javaschoolproject/entity/SupplementEntity.java
+++ src/main/java/com/firstofthekind/javaschoolproject/entity/SupplementEntity.java
@@ -15,7 +15,7 @@ import java.util.Set;
 @Getter
 @Setter
 @RequiredArgsConstructor
-public class SupplementEntity extends AbstractEntity{
+public class SupplementEntity extends AbstractEntity {
 
     @NotBlank
     private String title;
@@ -40,4 +40,49 @@ public class SupplementEntity extends AbstractEntity{
     )
     private Set<SupplementEntity> codependents = new HashSet<>();
 
+    public void addIncompatible(SupplementEntity sup) {
+        incompatible.add(sup);
+    }
+
+    public void deleteIncompatible(SupplementEntity sup) {
+        incompatible.removeIf(option1 -> option1.getId() == sup.getId());
+    }
+
+    public void addCodependent(SupplementEntity sup) {
+        codependents.add(sup);
+    }
+
+    public void deleteCodependent(SupplementEntity sup) {
+        codependents.removeIf(o -> o.getId() == sup.getId());
+    }
+
+    public boolean isCompatibleWith(Set<SupplementEntity> added) {
+        if (this.incompatible.isEmpty()) {
+            return true;
+        }
+        for (SupplementEntity sup :
+                added) {
+            if (sup.getIncompatible()
+                    .stream()
+                    .anyMatch(o -> o.getId() == this.getId())) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isDependentFrom(Set<SupplementEntity> added) {
+        if (this.codependents.isEmpty()) {
+            return true;
+        }
+        for (SupplementEntity sup:
+                this.getCodependents()) {
+            if (added
+                    .stream()
+                    .noneMatch(o -> o.getId() == sup.getId())) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/main/java/com/firstofthekind/javaschoolproject/service/SupplementService.java
+++ src/main/java/com/firstofthekind/javaschoolproject/service/SupplementService.java
@@ -2,14 +2,14 @@ package com.firstofthekind.javaschoolproject.service;
 
 
 import com.firstofthekind.javaschoolproject.dto.SupplementDto;
-import com.firstofthekind.javaschoolproject.dto.TariffDto;
 import com.firstofthekind.javaschoolproject.entity.SupplementEntity;
 import com.firstofthekind.javaschoolproject.entity.TariffEntity;
+import com.firstofthekind.javaschoolproject.exception.CodependentSupplementException;
+import com.firstofthekind.javaschoolproject.exception.IncompatibleSupplementException;
 import com.firstofthekind.javaschoolproject.repository.SupplementRepository;
 import com.firstofthekind.javaschoolproject.utils.ObjectMapperUtils;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
-import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -58,14 +58,113 @@ public class SupplementService {
     }
 
     @Transactional
-    public List<SupplementEntity> getAllAvailableToTariff(TariffEntity tariff) {
-        List<SupplementEntity> tariffSupplements = tariff.getSupplement();
-        List<SupplementEntity> allSupplements = supplementRepository.findAll();
+    public LinkedList<SupplementDto> getAllAvailableToTariff(TariffEntity tariff) {
+        LinkedList<SupplementEntity> tariffSupplements = tariff.getSupplement();
+        LinkedList<SupplementEntity> allSupplements = supplementRepository.findAll();
         for (SupplementEntity supplement : tariffSupplements) {
             allSupplements.removeIf(s -> s.getId() == supplement.getId());
         }
-        return allSupplements;
+        return ObjectMapperUtils.mapAll(allSupplements, SupplementDto.class);
     }
 
+    @Transactional
+    public void addIncompatible(long firstSupId,
+                                long secondSupId) throws CodependentSupplementException {
+        SupplementEntity first = supplementRepository.getById(firstSupId);
+        SupplementEntity second = supplementRepository.getById(secondSupId);
+        if (first.getCodependents().stream().noneMatch(o -> o.getId() == second.getId()) &&
+                second.getCodependents().stream().noneMatch(o -> o.getId() == first.getId())) {
+            first.addIncompatible(second);
+            second.addIncompatible(first);
+            supplementRepository.save(first);
+            supplementRepository.save(second);
+            log.info("Supplement " + first.getTitle() + " and " + second.getTitle() + " set as incompatible");
+        } else {
+            log.info("Error. Supplements " + first.getTitle() + " and " +
+                    second.getTitle() + " are codependent. ");
+            throw new CodependentSupplementException(
+                    "Error. Supplements " + first.getTitle() + " and " +
+                            second.getTitle() + " are codependent.");
+        }
+    }
+
+    @Transactional
+    public void deleteIncompatible(long firstSupId,
+                                   long secondSupId) {
+        SupplementEntity first = supplementRepository.getById(firstSupId);
+        SupplementEntity second = supplementRepository.getById(secondSupId);
+        first.deleteIncompatible(second);
+        second.deleteIncompatible(first);
+        supplementRepository.save(first);
+        supplementRepository.save(second);
+        log.info("Supplements " + first.getTitle() + " and " + second.getTitle() + " now is compatible");
+    }
+
+    @Transactional
+    public Set<SupplementEntity> getIncompatible(long SupplementId) {
+        SupplementEntity Supplement = supplementRepository.getById(SupplementId);
+        return Supplement.getIncompatible();
+    }
+
+    @Transactional
+    public LinkedList<SupplementEntity> getCompatible(long SupplementId) {
+        LinkedList<SupplementEntity> compatible = supplementRepository.findAll();
+        Set<SupplementEntity> incompatible = supplementRepository.getById(SupplementId).getIncompatible();
+        if (incompatible != null) {
+            for (SupplementEntity o :
+                    incompatible) {
+                long supId = o.getId();
+                compatible.removeIf(Supplement -> Supplement.getId() == supId);
+            }
+        }
+        compatible.removeIf(o -> o.getId() == SupplementId);
+        return compatible;
+    }
+
+    @Transactional
+    public void addDependent(long firstSupplementId,
+                             long secondSupplementId) throws IncompatibleSupplementException {
+        SupplementEntity first = supplementRepository.getById(firstSupplementId);
+        SupplementEntity second = supplementRepository.getById(secondSupplementId);
+        if (first.getIncompatible().stream().noneMatch(o -> o.getId() == second.getId())) {
+            first.addCodependent(second);
+            supplementRepository.save(first);
+            log.info("Supplement " + first.getTitle() + " codependent with " + second.getTitle());
+        } else {
+            log.info("Error. Supplement " + first.getTitle() + " is incompatible with "
+                    + second.getTitle() + " and can't be codependent");
+            throw new IncompatibleSupplementException(
+                    "Error. Supplement " + first.getTitle() + " is incompatible with "
+                            + second.getTitle() + " and can't be codependent");
+        }
+    }
+
+    @Transactional
+    public void deleteDependent(long firstSupplementId,
+                                long secondSupplementId) {
+        SupplementEntity first = supplementRepository.getById(firstSupplementId);
+        SupplementEntity second = supplementRepository.getById(secondSupplementId);
+        first.deleteCodependent(second);
+        supplementRepository.save(first);
+        log.info("Error. Supplement " + first.getTitle() + " is not codependent with " + second.getTitle());
+    }
+
+    @Transactional
+    public Set<SupplementEntity> getDependentSupplements(long supplementId) {
+        SupplementEntity supplement = supplementRepository.getById(supplementId);
+        return supplement.getCodependents();
+    }
+
+    @Transactional
+    public List<SupplementEntity> getIndependentSupplements(long supplementId) {
+        List<SupplementEntity> independent = supplementRepository.findAll();
+        Set<SupplementEntity> codependent = supplementRepository.getById(supplementId).getCodependents();
+        for (SupplementEntity supplement :
+                codependent) {
+            independent.removeIf(o -> o.getId() == supplement.getId());
+        }
+        independent.removeIf(o -> o.getId() == supplementId);
+        return independent;
+    }
 
 }
